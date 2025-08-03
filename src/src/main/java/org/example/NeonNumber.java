ec7f949cdea897d2bdd7f74a93de10c98685312f
palakpriyam <p.priyam@accenture.com>
2025-08-03 17:17:09 +0300

    Basic java programmes - 5

---
+++ src/src/main/java/org/example/NeonNumber.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NeonNumber {
+
+    public static void main(String[] args){
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        num = scanner.nextInt();
+        scanner.close();
+        neonNumberChecker(num);
+    }
+
+    static void neonNumberChecker(int n) {
+        int sq = n * n;
+        int sum = 0;
+        int sum2 = 0;
+        int digit = String.valueOf(n).length();
+        int originalNum = n;
+
+        while (sq != 0) {
+            int i = sq % 10;
+            sum = sum + i;
+            sq = sq / 10;
+        }
+
+        while (n != 0) {
+            if (digit > 1) {
+                sum2 += n % 10;
+                n = n / 10;
+            }
+        }
+
+        if (sum == sum2)
+            System.out.println(originalNum + " is a neon number");
+        else
+            System.out.println(originalNum + " is not a neon number");
+    }
+}
