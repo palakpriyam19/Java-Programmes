c5a0d7ab1be5177c4a6344671446b87d1da9d09b
palakpriyam <p.priyam@accenture.com>
2025-08-03 20:11:09 +0300

    Add Java String Manipulation programmes

--- src/src/main/java/org/example/stringProgrammes/StringReversal.java
+++ src/src/main/java/org/example/stringProgrammes/StringReversal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StringReversal {
 
     public static void main(String[] args){
-        String toBeReversed = null;
+        String toBeReversed;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a String: ");
         toBeReversed = scanner.nextLine();
