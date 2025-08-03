6acf5bf477621bd73d604d288aa400d281b83b35
palakpriyam <p.priyam@accenture.com>
2025-08-03 15:09:52 +0300

    Basic java programmes - 4

---
+++ src/src/main/java/org/example/ArmstrongNumber.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args){
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        num = scanner.nextInt();
+        scanner.close();
+
+        int result = 0;
+        int originalNumber = num; // 153
+        int noOfDigits = String.valueOf(num).length(); // Calculate the number of digits, 3
+
+        while (num != 0) {
+            int digit = num % 10;
+            result += (int) Math.pow(digit, noOfDigits);
+            num = num / 10;
+        }
+
+        if (originalNumber == result) {
+            System.out.println( originalNumber + " is an armstrong number");
+        } else {
+            System.out.println( originalNumber + " is NOT an armstrong number");
+        }
+
+    }
+
+}
